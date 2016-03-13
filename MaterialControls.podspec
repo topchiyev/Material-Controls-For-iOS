5ecae337b82db66ee4de32f79100509158f0bc71
Pasha L. Topchiyev <topchiyev@gmail.com>
2016-03-13 15:52:38 +0400

    Added header file for Swift compatibility

--- MaterialControls.podspec
+++ MaterialControls.podspec
@@ -1,13 +1,13 @@
 Pod::Spec.new do |s|
   s.name             = "MaterialControls"
-  s.version          = "1.1.0"
+  s.version          = "1.1.1"
   s.summary          = "Material Design controls for iOS writen in Objective-C."
 
-  s.homepage         = "https://github.com/fpt-software/Material-Controls-For-iOS"
+  s.homepage         = "https://github.com/topchiyev/Material-Controls-For-iOS"
   # s.screenshots     = "www.example.com/screenshots_1", "www.example.com/screenshots_2"
   s.license          = 'MIT'
   s.author           = { "FPT Software" => "mobility@fsoft.com.vn" }
-  s.source           = { :git => "https://github.com/fpt-software/Material-Controls-For-iOS.git", :tag => "1.0.2" }
+  s.source           = { :git => "https://github.com/topchiyev/Material-Controls-For-iOS.git", :tag => "1.0.4" }
 
   s.platform     = :ios, '7.0'
   s.requires_arc = true
---
+++ iOSUILib/iOSUILib/MaterialControls.h
@@ -0,0 +1,77 @@
+//
+//  MaterialControls.h
+//  iOSUILibDemo
+//
+//  Created by Pasha L. Topchiyev on 3/13/16.
+//  Copyright © 2016 FPT Software. All rights reserved.
+//
+
+#ifndef MaterialControls_header
+#define MaterialControls_header
+
+#import "AutoResizeTextView.h"
+#import "MDButton.h"
+#import "MDCollectionViewCell.h"
+#import "MDConstants.h"
+#import "MDProgress.h"
+#import "MDRippleLayer.h"
+#import "MDSlider.h"
+#import "MDSnackbar.h"
+#import "MDSwitch.h"
+#import "MDTabBar.h"
+#import "MDTabBarViewController.h"
+#import "MDTableViewCell.h"
+#import "MDTextField.h"
+#import "MDToast.h"
+#import "MDTouchGestureRecognizer.h"
+#import "UIView+MDExtension.h"
+
+
+
+//Calendar
+#import "MDCalendar.h"
+#import "MDCalendarCell.h"
+#import "MDCalendarDateHeader.h"
+#import "MDCalendarHeader.h"
+#import "MDCalendarYearSelector.h"
+#import "MDCalendarYearSelectorViewCell.h"
+#import "MDDatePicker.h"
+#import "MDDatePickerDialog.h"
+#import "MDTimePickerDialog.h"
+#import "NSCalendarHelper.h"
+#import "NSDate+MDExtension.h"
+
+
+
+//Helpers
+#import "MDDeviceHelper.h"
+#import "MDMathHelper.h"
+#import "NSDateHelper.h"
+#import "UIColorHelper.h"
+#import "UIFontHelper.h"
+#import "UIViewHelper.h"
+
+
+
+//Progress
+#import "MDCircularProgressLayer.h"
+#import "MDLinearProgressLayer.h"
+#import "MDProgressLayer.h"
+
+
+
+//Slider
+#import "MDBubbleLabel.h"
+#import "MDSliderIcon.h"
+#import "MDSliderThumbView.h"
+#import "MDSliderTickMarksView.h"
+
+
+
+//SuggestPopup
+#import "MDSuggestPopupView.h"
+#import "MDSuggestTableView.h"
+
+
+
+#endif /* MaterialControls_header */
